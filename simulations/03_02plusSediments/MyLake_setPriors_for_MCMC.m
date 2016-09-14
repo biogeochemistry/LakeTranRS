d67d4e07a5e0653622d65387729a5e37d62b3b12
kojitominaga <koji.tominaga@ibv.uio.no>
2016-09-14 21:37:51 +0800

    mass saving before updating submodules

---
+++ simulations/03_02plusSediments/MyLake_setPriors_for_MCMC.m
@@ -0,0 +1,53 @@
+K_values_sediments ...
+    = {0,   'K_OM1'; 	% optimized
+    0,      'K_OM2'; 		% optimized
+    0.0123, 'Km_O2'; 	% optimized 
+    0.01,   'Km_NO3'; 	% not varied
+    3.92,   'Km_Fe(OH)3';% optimized
+    2415,   'Km_FeOOH'; 	% optimized
+    0.0293, 'Km_SO4'; 	% not varied
+    0.001,  'Km_O2ao'; 	% optimized
+    0.1,    'Km_NH4ao'; 	% not varied
+    0.0219, 'Kin_O2'; 	% 1e-2, 1
+    0.1,    'Kin_NO3'; 	% not varied
+    0.1,    'Kin_FeOH3'; 	% not varied
+    0.1,    'Kin_FeOOH'; 	% not varied
+    100,    'K_NH4ox'; 	% not varied
+    8.7e4,  'K_Feox'; 	% optimized
+    0.1,    'K_Sdis'; 	% not varied
+    0.1,    'K_Spre'; 	% not varied
+    3.17,   'K_FeS2pre'; 	% not varied
+    0.1,    'K_AlOH3'; 	% not varied
+    1.35,   'K_P_sorb_a'; 	% not varied
+    1.35,   'K_P_sorb_b'; 	% not varied
+    6500,   'K_rhom'; 	% not varied
+    0.1,    'K_tS_Fe';  	% not varied
+    2500,   'K_Fe_S'; 	% not varied
+    0.001,  'K_Fe_dis'; 	% not varied
+    21.3,   'K_Fe_pre'; 	% not varied
+    0.37,   'K_apa'; 		% not varied
+    3e-6,   'Kapa'; 	% not varied
+    8e-2,   'K_sorg'; 	% not varied
+    1000,   'K_tsox'; 	% not varid
+    0.001,  'K_FeS_FeS2'; 	% not varied
+    0,      'accel'};		% , 0.01-50 #1
+
+K_values_lake =      ...
+   {0,      'I_scT'; 	% 
+    1,      'I_scDOC'; 	% 
+    0.2,    'w_s'; 		% 
+    0.2,    'w_chl';    % 
+    1,      'Y_cp'      %
+    0.2,    'm_twty'    %
+    1.5     'g_twty'     %
+    2e-4    'k_sed_twty' %
+    0       'k_dop_twty' %    
+    0.2     'P_half'     %
+    0.01,   'oc_DOC'; 	 % 
+    0.1,    'qy_DOC'; 	 % 
+    0.1,    'k_BOD'; 	 % 
+    1.05,   'theta_bod';	 % 
+    1.13,   'theta_bod_ice'; 	% 
+    4,      'theta_T';     % 
+    1,      'I_scO'}; 	% 
+
