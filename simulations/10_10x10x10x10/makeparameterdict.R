c350ae8d983ace704af515e57b57a964fcaef337
kojitominaga <koji.tominaga@ibv.uio.no>
2017-03-02 20:03:42 +0800

    10 initial files copied from 08

---
+++ simulations/10_10x10x10x10/makeparameterdict.R
@@ -0,0 +1,22 @@
+## this file creates the file intermediate/parameterdict.csv
+## -- provides the combinations of the parameter levels
+## -- convenient because I want to avoid telling Matlab parameter settings
+##    instead of telling parameter settings, just give Matlab the folders and run it
+
+
+n1 <- 5
+n2 <- 5
+n3 <- 5
+n4 <- 5
+
+x1 <- 1:n1
+x2 <- 1:n2
+x3 <- 1:n3
+x4 <- 1:n4
+
+d <- expand.grid(x1, x2, x3, x4)
+d[['simid']] <- 1:nrow(d)
+
+write.csv(d, file='intermediate/parameterdict.csv',
+          row.names=FALSE)
+
