7b2ed650af8a920054b48213e4756a198163c683
kojitominaga <koji.tominaga@ibv.uio.no>
2017-03-08 08:32:09 +0800

    changed id from 5555 to 9999

--- simulations/10_9x9x9x9/postprocessing/sim_specific.py
+++ simulations/10_9x9x9x9/postprocessing/sim_specific.py
@@ -10,8 +10,8 @@ sns.set_style('whitegrid')
 # bz2 = '.bz2'  
 bz2 = ''
 
-basedir = '../simulations/id/004445/'
-inbasedir = '../intermediate/id/004445/'
+basedir = '../simulations/id/003281/'  # 5,5,5,5
+inbasedir = '../intermediate/id/003281/'
 
 design = pd.read_csv('../intermediate/parameterdict.csv')
 bath = pd.read_csv('../bathymetry05.csv', header=None)
@@ -336,52 +336,42 @@ if not os.path.exists('sim_specific'):
 
     
 
-# sns.set_palette('coolwarm', 2)
-# plotsim([311, 315], 'results_raw/Air Temperature.png',
-#         'impact of air temperature')
+sns.set_palette('coolwarm', 2)
+plotsim([3277, 3285], 'results_raw/AT.png',
+        'impact of air temperature')
 
-# sns.set_palette('Reds_r', 2)
-# plotsim([303, 323], 'results_raw/Wind Speed.png', 
-#         'impact of wind speed')
+sns.set_palette('Reds_r', 2)
+plotsim([3245, 3317], 'results_raw/WS.png', 
+        'impact of wind speed')
 
-# sns.set_palette('Greens_r', 2)
-# plotsim([263, 363], 'results_raw/Total P.png',
-#         'impact of total P loading')
+sns.set_palette('Greens_r', 2)
+plotsim([2957, 3605], 'results_raw/TP.png',
+        'impact of total P loading')
 
-# sns.set_palette('Reds', 2)
-# plotsim([63, 563], 'results_raw/DOC.png',
-#              'impact of DOC loading')
+sns.set_palette('Reds', 2)
+plotsim([365, 6197], 'results_raw/DOC.png',
+             'impact of DOC loading')
 
 
-# sns.set_palette('Oranges_d', 1)
-# plotsim(311, 'results_raw/AT colder.png', 
-#         '"lower air temperature" compared to "base"')
-# plotsim(315, 'results_raw/AT warmer.png',
-#         '"higher air temperature" compared to "base"')
-# plotsim(303, 'results_raw/WS calmer.png',
-#         '"calmer wind compared" to "base"')
-# plotsim(323, 'results_raw/WS stronger.png',
-#         '"stronger wind compared" to "base"')
-# plotsim(263, 'results_raw/TP lower.png', 
-#         '"less TP loading compared" to "base"')
-# plotsim(363, 'results_raw/TP higher.png',
-#         '"greater TP loading compared" to "base"')
-# plotsim(63, 'results_raw/DOC lower.png',
-#         '"less DOC loading compared" to "base"')
-# plotsim(563, 'results_raw/DOC higher.png',
-#         '"greater DOC loading compared" to "base"')
+sns.set_palette('Oranges_d', 1)
+plotsim(3227, 'results_raw/AT colder.png', 
+        '"lower air temperature" compared to "base"')
+plotsim(3285, 'results_raw/AT warmer.png',
+        '"higher air temperature" compared to "base"')
+plotsim(3245, 'results_raw/WS calmer.png',
+        '"calmer wind compared" to "base"')
+plotsim(3317, 'results_raw/WS stronger.png',
+        '"stronger wind compared" to "base"')
+plotsim(2957, 'results_raw/TP lower.png', 
+        '"less TP loading compared" to "base"')
+plotsim(3605, 'results_raw/TP higher.png',
+        '"greater TP loading compared" to "base"')
+plotsim(365, 'results_raw/DOC lower.png',
+        '"less DOC loading compared" to "base"')
+plotsim(6197, 'results_raw/DOC higher.png',
+        '"greater DOC loading compared" to "base"')
 
 # plotsim(1, 'sim_specific/test001.png', '"low in everything" compared to "base"')
 # plotsim(505, 'sim_specific/test505.png', '"id505" compared to "base"')
-# plotsim(5, 'sim_specific/test005.png', '"id005" compared to "base"')
-# plotsim(105, 'sim_specific/test105.png', '"id105" compared to "base"')
-# plotsim(605, 'sim_specific/test605.png', '"id605" compared to "base"')
-# plotsim(101, 'sim_specific/test101.png', '"id101" compared to "base"')
-# plotsim(601, 'sim_specific/test601.png', '"id601" compared to "base"')
-# plotsim(501, 'sim_specific/test501.png', '"id501" compared to "base"')
-# plotsim(21, 'sim_specific/test021.png', '"id021" compared to "base"')
-# plotsim(121, 'sim_specific/test121.png', '"id121" compared to "base"')
-# plotsim(521, 'sim_specific/test521.png', '"id521" compared to "base"')
-# plotsim(621, 'sim_specific/test621.png', '"id621" compared to "base"')
 
 
