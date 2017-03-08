3fa9f2bea0ba701b92f367a3200d0c3e78d4ebb0
kojitominaga <koji.tominaga@ibv.uio.no>
2017-03-08 08:48:17 +0800

    5555to9999, O2zt and real thresholds, rm cols not needed

--- simulations/10_9x9x9x9/postprocessing/pp.py
+++ simulations/10_9x9x9x9/postprocessing/pp.py
@@ -11,13 +11,13 @@ bz2 = ''
 bath = pd.read_csv('../bathymetry05.csv', header=None)
 bath.columns = ['zz', 'Az']
 
-lambdai = 5   # m-1
-lambdas = 15  # m-1
+# lambdai = 5   m-1
+# lambdas = 15  m-1
 
-n1 = 10
-n2 = 10
-n3 = 10
-n4 = 10
+n1 = 9
+n2 = 9
+n3 = 9
+n4 = 9
 nt = 2922
 nz = 18
 if nz == 18:
@@ -26,13 +26,16 @@ if nz == 18:
     dz = 0.5
 else:
     error 
-nr = 11 # responses
+
+## these two below in the end should become consistent
+nr = 6 # responses (used for plotting)
+nrold = 11 # responses old version (used to keep the matrix collection)
 
 ser = pd.period_range('2010-01-01', periods=(365*4+1)*2)
 
-a = np.ndarray((n1, n2, n3, n4, nr), dtype=np.float) * np.nan
-a0 = np.ndarray((n1, n2, n3, n4, nr), dtype=np.float) * np.nan
-m = np.zeros((n1, n2, n3, n4, nr), dtype=bool) 
+a = np.ndarray((n1, n2, n3, n4, nrold), dtype=np.float) * np.nan
+a0 = np.ndarray((n1, n2, n3, n4, nrold), dtype=np.float) * np.nan
+m = np.zeros((n1, n2, n3, n4, nrold), dtype=bool) 
 
 d = pd.read_csv('../intermediate/parameterdict.csv')
 
@@ -53,7 +56,7 @@ for i, x1, x2, x3, x4, id in d.itertuples():
     chl.index = ser
     # tp = pd.read_csv(os.path.join(di, 'totp.csv{:s}'.format(bz2)), header=None)
     # tp.index = ser
-    o2 = pd.read_csv(os.path.join(di, 'O2abs.csv{:s}'.format(bz2)), header=None)
+    o2 = pd.read_csv(os.path.join(di, 'O2zt.csv{:s}'.format(bz2)), header=None)
     o2.index = ser
     his = pd.read_csv(os.path.join(di, 'His.csv{:s}'.format(bz2)), header=None)
     his.index = ser
@@ -64,19 +67,19 @@ for i, x1, x2, x3, x4, id in d.itertuples():
                       
 
     # number of anoxia (O2 < 0.05 at bottom) per year (last 4 years) def I
-    anoxia1 = (o2.loc[o2.index.year >= 2014].iloc[:, zi] < 0.05).sum() / 4 
+    anoxia1 = (o2.loc[o2.index.year >= 2014].iloc[:, zi] < 3000).sum() / 4 
 
     # number of anoxia (O2 < 0.2 at bottom) per year (last 4 years) def II
-    anoxia2 = (o2.loc[o2.index.year >= 2014].iloc[:, zi] < 0.2).sum() / 4 
+    anoxia2 = (o2.loc[o2.index.year >= 2014].iloc[:, zi] < 16).sum() / 4 
 
-    # number of anoxia (O2 < 0.4 at 2m at zim) per year (last 4 years) def III
-    anoxia3 = (o2.loc[o2.index.year >= 2014].iloc[:, zim] < 0.4).sum() / 4 
+    # # number of anoxia (O2 < 0.4 at 2m at zim) per year (last 4 years) def III
+    # anoxia3 = (o2.loc[o2.index.year >= 2014].iloc[:, zim] < 0.4).sum() / 4 
 
-    # mean O2 concentration at the bottom (last 4 years)
-    mo21 = o2.iloc[:, zi].mean()
+    # # mean O2 concentration at the bottom (last 4 years)
+    # mo21 = o2.iloc[:, zi].mean()
 
-    # mean O2 concentration at 2m (last 4 years)
-    mo22 = o2.iloc[:, zim].mean()
+    # # mean O2 concentration at 2m (last 4 years)
+    # mo22 = o2.iloc[:, zim].mean()
     
     # mean annual maximum chl at surface (last 4 years)
     amc = chl.iloc[:, 0].groupby(chl.index.year).max()
@@ -89,14 +92,14 @@ for i, x1, x2, x3, x4, id in d.itertuples():
     # number of ice covered days per year (last 4 years)
     ic = (his.loc[his.index.year >= 2014].iloc[:, 0] > 0).sum() / 4
     
-    # mean MAM irradiance at middle (last 4 years)
-    # mean JJA irradiance at middle (last 4 years)
-    ir00 = qst.sw * np.exp(-zm * lam.iloc[:, zim])
-    ir00.index = ser
-    ir1 = ir00[(ir00.index.month > 2) & (ir00.index.month < 6) &
-               (ir00.index.year >= 2014)].mean()
-    ir2 = ir00[(ir00.index.month > 5) & (ir00.index.month < 9) &
-               (ir00.index.year >= 2014)].mean()
+    # # mean MAM irradiance at middle (last 4 years)
+    # # mean JJA irradiance at middle (last 4 years)
+    # ir00 = qst.sw * np.exp(-zm * lam.iloc[:, zim])
+    # ir00.index = ser
+    # ir1 = ir00[(ir00.index.month > 2) & (ir00.index.month < 6) &
+    #            (ir00.index.year >= 2014)].mean()
+    # ir2 = ir00[(ir00.index.month > 5) & (ir00.index.month < 9) &
+    #            (ir00.index.year >= 2014)].mean()
 
     # mixing depth -- mean JJ (last 4 years)
     pycnothresholdunit = 0.1 ## as per MyLake, kg m-3 m-1
@@ -123,14 +126,14 @@ for i, x1, x2, x3, x4, id in d.itertuples():
 
     a0[x1-1, x2-1, x3-1, x4-1, 0] = anoxia1
     a0[x1-1, x2-1, x3-1, x4-1, 1] = anoxia2
-    a0[x1-1, x2-1, x3-1, x4-1, 2] = anoxia3
-    a0[x1-1, x2-1, x3-1, x4-1, 3] = mo21
-    a0[x1-1, x2-1, x3-1, x4-1, 4] = mo22
+    # a0[x1-1, x2-1, x3-1, x4-1, 2] = anoxia3
+    # a0[x1-1, x2-1, x3-1, x4-1, 3] = mo21
+    # a0[x1-1, x2-1, x3-1, x4-1, 4] = mo22
     a0[x1-1, x2-1, x3-1, x4-1, 5] = mamc * 1e3
     a0[x1-1, x2-1, x3-1, x4-1, 6] = chlJJA * 1e3
     a0[x1-1, x2-1, x3-1, x4-1, 7] = ic
-    a0[x1-1, x2-1, x3-1, x4-1, 8] = ir1
-    a0[x1-1, x2-1, x3-1, x4-1, 9] = ir2
+    # a0[x1-1, x2-1, x3-1, x4-1, 8] = ir1
+    # a0[x1-1, x2-1, x3-1, x4-1, 9] = ir2
     a0[x1-1, x2-1, x3-1, x4-1, 10] = mdepJJ
     
     
@@ -145,8 +148,8 @@ a = ma.masked_array(a0, mask=m)
 
 
 ## contour preparation
-x = np.arange(10)
-y = np.arange(10)
+x = np.arange(9)
+y = np.arange(9)
 X, Y = np.meshgrid(x, y)
 
 
@@ -159,11 +162,11 @@ def rs(ax, thisa, cmapname, thismin, thismax, thisfmt, label1st, label2nd):
         ax.clabel(cont, infline=1, fontsize=8, colors='black', fmt=thisfmt)
     # ax.set_xlabel(label2nd)
     # ax.set_ylabel(label1st)
-    ax.text(4.5, -0.9, label2nd, ha='center', va='center') ; 
-    ax.text(-0.6, 4.5, label1st, va='center', ha='right', rotation='vertical')
+    ax.text(4.0, -0.9, label2nd, ha='center', va='center') ; 
+    ax.text(-0.6, 4.0, label1st, va='center', ha='right', rotation='vertical')
 
 def plotrs6(rsi, nr, aa, colorcode, fmt, n) :
-    '''returns 6 axes'''
+    '''returns 6 axes. rsi is the column in the RS plot'''
     axes = [plt.subplot2grid((6, nr), (i, rsi)) for i in range(6)]
     mi = np.nanmin(aa)
     ma = np.nanmax(aa)
@@ -185,26 +188,26 @@ n = ['air temp', 'wind speed', 'total P', 'DOC']
 
 aa0 = plotrs6(0, nr, a[:, :, :, :, 0], 'Blues', '%.f', n)
 aa1 = plotrs6(1, nr, a[:, :, :, :, 1], 'Blues', '%.f', n)
-aa2 = plotrs6(2, nr, a[:, :, :, :, 2], 'Blues', '%.f', n)
-aa3 = plotrs6(3, nr, a[:, :, :, :, 3], 'Purples', '%.2f', n)
-aa4 = plotrs6(4, nr, a[:, :, :, :, 4], 'Purples', '%.2f', n)
-aa5 = plotrs6(5, nr, a[:, :, :, :, 5], 'Greens', '%.f', n)
-aa6 = plotrs6(6, nr, a[:, :, :, :, 6], 'Greens', '%.f', n)
-aa7 = plotrs6(7, nr, a[:, :, :, :, 7], 'Reds', '%.f', n)
-aa8 = plotrs6(8, nr, a[:, :, :, :, 8], 'Greys_r', '%.f', n)
-aa9 = plotrs6(9, nr, a[:, :, :, :, 9], 'Greys_r', '%.2f', n)
-aa10 = plotrs6(10, nr, a[:, :, :, :, 10], 'Oranges', '%.1f', n)
+# aa2 = plotrs6(_, nr, a[:, :, :, :, 2], 'Blues', '%.f', n)
+# aa3 = plotrs6(_, nr, a[:, :, :, :, 3], 'Purples', '%.2f', n)
+# aa4 = plotrs6(_, nr, a[:, :, :, :, 4], 'Purples', '%.2f', n)
+aa5 = plotrs6(2, nr, a[:, :, :, :, 5], 'Greens', '%.f', n)
+aa6 = plotrs6(3, nr, a[:, :, :, :, 6], 'Greens', '%.f', n)
+aa7 = plotrs6(4, nr, a[:, :, :, :, 7], 'Reds', '%.f', n)
+# aa8 = plotrs6(_, nr, a[:, :, :, :, 8], 'Greys_r', '%.f', n)
+# aa9 = plotrs6(_, nr, a[:, :, :, :, 9], 'Greys_r', '%.2f', n)
+aa10 = plotrs6(5, nr, a[:, :, :, :, 10], 'Oranges', '%.1f', n)
 
 aa0[0].set_title('anoxia d y-1\nbottom')
 aa1[0].set_title('anoxia d y-1\nbottom alt')
-aa2[0].set_title('anoxia d y-1\nmiddle')
-aa3[0].set_title('mean [O2]\nbottom')
-aa4[0].set_title('mean [O2]\nmiddle')
+# aa2[0].set_title('anoxia d y-1\nmiddle')
+# aa3[0].set_title('mean [O2]\nbottom')
+# aa4[0].set_title('mean [O2]\nmiddle')
 aa5[0].set_title('mean annual\nmax chl\nsurface')
 aa6[0].set_title('mean JJA chl\nsurface')
 aa7[0].set_title('ice cover\ndays y-1')
-aa8[0].set_title('mean MAM\nirradiance\nmiddle')
-aa9[0].set_title('mean JJA\nirradiance\nmiddle')
+# aa8[0].set_title('mean MAM\nirradiance\nmiddle')
+# aa9[0].set_title('mean JJA\nirradiance\nmiddle')
 aa10[0].set_title('mean JJ\nmixing depth')
 
 for ax in fig.get_axes():
@@ -214,5 +217,5 @@ for ax in fig.get_axes():
 
 fig.set_figheight(10)
 fig.set_figwidth(18)
-fig.savefig('RSver2.png', dpi=150, bbox_inches='tight')
-fig.savefig('RSver2lowres.png', dpi=75, bbox_inches='tight')
+fig.savefig('RSver3.png', dpi=150, bbox_inches='tight')
+fig.savefig('RSver3lowres.png', dpi=75, bbox_inches='tight')
