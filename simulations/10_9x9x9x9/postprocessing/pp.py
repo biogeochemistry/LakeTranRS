c9e15afe8683f79e26a83205add2d1a02124fa08
kojitominaga <koji.tominaga@ibv.uio.no>
2017-03-10 16:20:49 +0800

    label adjustments

--- simulations/10_9x9x9x9/postprocessing/pp.py
+++ simulations/10_9x9x9x9/postprocessing/pp.py
@@ -185,9 +185,9 @@ def plotrs6(rsi, nr, aa, colorcode, fmt, fmt3, n) :
     rs(axes[3], aa[4, :, :, 4], colorcode, mi, ma, fmt, n[1], n[2])
     rs(axes[4], aa[4, :, 4, :], colorcode, mi, ma, fmt, n[1], n[3])
     rs(axes[5], aa[4, 4, :, :], colorcode, mi, ma, fmt, n[2], n[3])
-    axes[5].text(-0.5, -2.4, 'min: ' + fmt3.format(mi), 
+    axes[5].text(-0.5, -3.0, 'min: ' + fmt3.format(mi), 
                  fontsize=10, ha='left', va='center')
-    axes[5].text(-0.5, -3.6, 'max: ' + fmt3.format(ma), 
+    axes[5].text(-0.5, -4.5, 'max: ' + fmt3.format(ma), 
                  fontsize=10, ha='left', va='center')
     return axes
 
@@ -226,7 +226,7 @@ for ax in fig.get_axes():
     ax.get_yaxis().set_visible(False)
     ax.set_axis_bgcolor('black')
 
-fig.set_figheight(10)
+fig.set_figheight(9.5)
 fig.set_figwidth(9)
 fig.savefig('RSver3.png', dpi=150, bbox_inches='tight')
 fig.savefig('RSver3lowres.png', dpi=75, bbox_inches='tight')
